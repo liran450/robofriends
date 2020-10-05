4d7a1eac3a3a87080e3ff25256d28649b0b6ae0c
unknown <liran450@gmail.com>
2020-10-05 16:24:26 +0300

    Updates

---
+++ precache-manifest.53e69e6f891291de078d34d9691c05ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7064a01fc1e6fe11b27f42680468f95a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a36fe57a59c46220c148",
+    "url": "/static/css/main.7f2a7e61.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36fe57a59c46220c148",
+    "url": "/static/js/main.d8bbba23.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
