a7ddd8b7e623befa3f3a212fbe1017977ab32775
unknown <liran450@gmail.com>
2020-10-06 23:41:47 +0300

    changing height for Searchbox input

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({searchBar}) => {
 return(
 	<div className='pa2'>
 	<input onChange={searchBar}
-	className='pa3 ba b--green bg-lightest-blue'
+	className='pa3 ba b--green bg-lightest-blue h-25'
 	type='search' 
 	placeholder='Search Robots' 
 	/>
